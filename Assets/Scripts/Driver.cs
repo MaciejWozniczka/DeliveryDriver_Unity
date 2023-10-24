b09302be4e0f567c2cf92d53617ec2deb4f47f61
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-10-24 23:41:45 +0200

    Car movement script added

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float moveSpeed = 5f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float steerAmount = steerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveAmount = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
