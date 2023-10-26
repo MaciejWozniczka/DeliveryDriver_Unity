586bdf0f0b000a078ef660c7b7f5d0f302523a0a
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-10-26 18:23:25 +0200

    Changed driver movement speed

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -14,7 +14,7 @@ public class Driver : MonoBehaviour
     void Update()
     {
         float steerAmount = steerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        float moveAmount = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        float moveAmount = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime * 2;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
