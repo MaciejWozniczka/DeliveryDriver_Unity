585f3f758caea7c02f28ed54ec21191224f11d2c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-03 17:42:24 +0100

    Changing car color when has package

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 0, 1); 
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1); 
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch! "+ other.gameObject.name);
     }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject);
         }
         
@@ -22,6 +33,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
