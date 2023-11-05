566752d46f77ed2ca15fb8b459fd3b6be094772a
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-05 23:25:22 +0100

    Added CodeMonkey utils

---
+++ Assets/Scripts/WindowsPointer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsPointer : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private RectTransform pointerRectTransform;
+
+    private void Awake()
+    {
+        targetPosition = new Vector3(-26.5f, 1.8f, 0);
+        pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+    }
+}
