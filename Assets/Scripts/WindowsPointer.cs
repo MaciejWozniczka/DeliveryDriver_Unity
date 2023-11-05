13c8c877dd8e4cfdfa364149c245a20efd2cb195
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-06 00:32:24 +0100

    Added script for pointing selected location

--- Assets/Scripts/WindowsPointer.cs
+++ Assets/Scripts/WindowsPointer.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
+using Unity.VisualScripting;
 
 public class WindowsPointer : MonoBehaviour
 {
+    [SerializeField] private Camera uiCamera;
     private Vector3 targetPosition;
     private RectTransform pointerRectTransform;
 
     private void Awake()
     {
-        targetPosition = new Vector3(-26.5f, 1.8f, 0);
+        targetPosition = new Vector3(36.22f, 6.72f, 0);
         pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
     }
 
@@ -20,5 +23,8 @@ public class WindowsPointer : MonoBehaviour
 
         fromPosition.z = 0f;
         Vector3 dir = (toPosition - fromPosition).normalized;
+
+        float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
     }
 }
