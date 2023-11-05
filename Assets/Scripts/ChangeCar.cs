b02385ca64b580dc777aeb31da0b5b9a96e68550
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-05 22:37:28 +0100

    Deleted message for changing car

--- Assets/Scripts/ChangeCar.cs
+++ Assets/Scripts/ChangeCar.cs
@@ -1,26 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class ChangeCar : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
-    private bool textIsDisplayed = false;
-    public TextMeshProUGUI textUI;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        if (textIsDisplayed)
-        {
-            textUI.text = "Zmieniono samochód!";
-            textUI.enabled = true;
-            StartCoroutine(HideTextAfterDelay());
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
@@ -30,16 +17,7 @@ public class ChangeCar : MonoBehaviour
             Debug.Log("Car changed!");
             spriteRenderer.sprite = other.gameObject.GetComponent<SpriteRenderer>().sprite;
             
-            textIsDisplayed = true;
-
             Destroy(other.gameObject);
         }
-    }
-
-    private IEnumerator HideTextAfterDelay()
-    {
-        yield return new WaitForSeconds(5f);
-        textUI.enabled = false;
-        textIsDisplayed = false;
     }
 }
