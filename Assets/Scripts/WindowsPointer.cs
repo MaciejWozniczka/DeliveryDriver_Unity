abfb2879a39277e6902e27eabe447891bb22904f
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-06 01:33:49 +0100

    Making WindowsPointer public

--- Assets/Scripts/WindowsPointer.cs
+++ Assets/Scripts/WindowsPointer.cs
@@ -7,16 +7,16 @@ using Unity.VisualScripting;
 public class WindowsPointer : MonoBehaviour
 {
     [SerializeField] private Camera uiCamera;
-    private Vector3 targetPosition;
+    public Vector3 targetPosition;
     private RectTransform pointerRectTransform;
 
-    private void Awake()
+    private void Start()
     {
-        targetPosition = new Vector3(36.22f, 6.72f, 0);
+        targetPosition = new Vector3(-7.8f, 3.6f, 0);
         pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
     }
 
-    private void Update()
+    public void Update()
     {
         Vector3 toPosition = targetPosition;
         Vector3 fromPosition = Camera.main.transform.position;
@@ -26,5 +26,7 @@ public class WindowsPointer : MonoBehaviour
 
         float angle = UtilsClass.GetAngleFromVectorFloat(dir);
         pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
+
+        Debug.Log("Pointer: " + targetPosition.x + ", " + targetPosition.y);
     }
 }
