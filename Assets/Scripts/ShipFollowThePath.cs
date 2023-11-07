560bb8cfe9ea69e151653bdc267fb9e2691616ce
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-07 17:33:20 +0100

    Added ShipFollowThePath script

---
+++ Assets/Scripts/ShipFollowThePath.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class ShipFollowThePath : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints;
+    float moveSpeed = 2f;
+    private int waypointIndex = 0;
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            Vector3 toPosition = waypoints[waypointIndex].transform.position;
+            Vector3 fromPosition = transform.position;
+
+            fromPosition.z = 0f;
+            Vector3 dir = (toPosition - fromPosition).normalized;
+
+            float angle = UtilsClass.GetAngleFromVectorFloat(dir);
+            transform.localEulerAngles = new Vector3(0, 0, angle - 90);
+            
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+}
